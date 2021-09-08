88844ec03aaf7cb41e1279321bdc040ef4e1e704
PedroVic12 <pedrovictor.rveras12@gmail.com>
2021-09-08 00:57:21 -0300

    Aula1

--- Curso-BackEnd-EstaçãoHack/projeto002/exemplo1.js
+++ Curso-BackEnd-EstaçãoHack/projeto002/exemplo1.js
@@ -1,1 +1,25 @@
-console.log('Hello World!')
+// REQUIRE => Fazer importaçoes de modulos, bibliotecas e entre outros
+
+//neste exemplo, estamos incorporando a biblioteca http ao nosso projeto
+
+
+const http = require('http')
+
+let server = http.createServer(function(req, res){
+    //criamos um servidor e passamos como argumento uma função anonima que vai receber uma requisição e uma resposta
+
+    //Normalmente a resposta de uma requisição http é um arquivo html
+
+    res.end("<html>  <head></head>  <body>  <h1> Minha primeira requisição com resposta NODE.JS </h1> </body>   </html>")
+
+    server.listen(3000, () => {
+        //fica escutando a porta 3000 aguardando uma requisição
+
+        console.log('Servidor escutando a porta 300')
+        console.log("Pressione CTRL+C para encerrar")
+
+    }) 
+
+
+
+})
--- Lista de exercicios/ex001.js
+++ Lista de exercicios/ex001.js
@@ -1,6 +1,8 @@
+console.log("Ola mundo")
+
 // LISTA DE EXERCICIOS EM JS
 
-//QUESTÃO 1)
+//--------QUESTÃO 1)
 var nome = "Pedro Victor"
 
 console.log('Ola ' + nome)
@@ -9,4 +11,68 @@ console.log('Ola ' + nome)
 
 console.log(`Ola ${nome} Bem vindo`)
 
-//QUESTÃO 2)
+
+
+
+//------------QUESTÃO 2)
+function media(n1,n2){
+
+    console.log(    (n1 + n2)/2   )
+}
+
+media(10,5)
+
+function media2(n3,n4){
+
+    return ( (n3+n4)/2  )
+}
+
+var resultado = media2(10,5)
+
+console.log(resultado)
+
+
+
+
+
+// ---------- QUESTAO 3)
+
+var frutas = ['laranja','limao', 'banana', 'maça', 'uva']
+
+console.log(frutas[2])
+
+
+// ---------- QUESTAO 5)
+
+function soma(n1,n2){
+
+    console.log( (n1+n2))
+}
+
+soma(6,4)
+
+function sub(n1,n2){
+
+    console.log( (n1-n2))
+
+}
+
+sub(6,4)
+
+function div(n1,n2){
+
+    console.log( (n1/n2))
+}
+
+div(10,5)
+
+
+// ---------- QUESTAO 6)
+
+
+
+
+
+
+
+
